6e4fcf1ec5a577d45c52fc2076917f1d3498323d
unknown <elgdiab@gmail.com>
2024-12-17 03:56:36 +0200

    Handling GET requests.

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
+const fs = require('node:fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
+//   res.status(200).json({
+//     message: 'Hello from the other side...',
+//     app: 'natourse',
+//   });
+// });
+
+// app.post('/', (req, res) => {
+//   res.status(200).send(`You can't post at that endpoind...`);
+// });
+
+// app.delete('/users', (req, res) => {
+//   res.status(200).send(`You can't delete at this endpoint...`);
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, 'utf-8')
+);
+
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    message: 'Hello from the other side...',
-    app: 'natourse',
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
   });
 });
 
-app.post('/', (req, res) => {
-  res.status(200).send(`You can't post at that endpoind...`);
-});
-
-const port = 8000;
+const port = 3000;
 
 app.listen(port, () => {
-  console.log(`App is working in port ${port}...`);
+  console.log(`App is working on port ${port}...`);
 });
