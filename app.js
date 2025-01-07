2a7970fb03d9d399aa400b2d298db97d223bc516
unknown <elgdiab@gmail.com>
2025-01-07 04:18:19 +0200

    Adding the global error handler and the app goabla error.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utilits/appError.js');
+const globalErrorHandler = require('./controllers/errorController.js');
 
 const app = express();
 
@@ -39,6 +41,22 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all(`*`, (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'Fail',
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // });
+
+  // const error = new Error(`Can't find ${req.originalUrl} on this server`);
+  // error.statusCode = 404;
+  // error.status = 'Fail';
+  // next(error);
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 // Server
 module.exports = app;
