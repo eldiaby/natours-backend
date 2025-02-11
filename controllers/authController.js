df70400f0e48899fbec734de016c243c1f4758d2
unknown <elgdiab@gmail.com>
2025-02-11 15:57:32 +0200

    Implement sending JWT as a cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,6 +18,21 @@ const createSendToken = (user, statusCode, res) => {
   // Create the token
   const token = sighToken(user._id);
 
+  // Create the cookie
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from output
+  user.password = undefined;
+
   // Send the data
   res.status(statusCode).json({
     status: 'Success',
