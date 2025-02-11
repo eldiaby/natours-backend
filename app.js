19b412ad479e6fef91196c7617feeb998c0ab6d5
unknown <elgdiab@gmail.com>
2025-02-11 16:46:43 +0200

    Implement data sanitization againt NoSQL query injection

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+// const xss = require('xss-clean');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -32,6 +34,12 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against
+// app.use(xss());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
