8dfa0697ebcc4d749cf27389da5d66492f0620f0
unknown <elgdiab@gmail.com>
2025-02-11 16:27:40 +0200

    Install hemlet middlewate and use it

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 
 const tourRouter = require('./routes/tourRoutes');
@@ -11,22 +12,31 @@ const app = express();
 
 // 1) Global Middlewars
 
+// Set securty HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requists from same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, Please try again in an hour!',
 });
-
 app.use('/api', limiter);
 
 // app.use(morgan('dev'));
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
 
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
