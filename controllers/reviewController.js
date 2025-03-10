3a37adacd8004d4252d90f56f65a298388af9f42
unknown <elgdiab@gmail.com>
2025-03-10 17:34:34 +0200

    Update getAllReviews function to get all reviews for current tour

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,7 +2,11 @@ const Review = require('./../module/reviewModel.js');
 const catchAsync = require('./../utilits/catchAsync.js');
 
 module.exports.gerAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+  // If there is a spacific tour then get all reviews for this tour else get all treviews in general
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+
+  const reviews = await Review.find(filter);
 
   res.status(200).json({
     status: 'success',
