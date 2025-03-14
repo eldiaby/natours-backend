5bdf8103943c5ea667a71c9a5ae03bd3ae9fbd93
unknown <elgdiab@gmail.com>
2025-03-14 17:23:50 +0200

    Prevent duplicate reviews

--- module/reviewModel.js
+++ module/reviewModel.js
@@ -34,6 +34,9 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+// To make a user make just one review for the same tour
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   // this.populate({
   //   path: 'tour',
